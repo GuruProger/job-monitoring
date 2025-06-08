70e801412301f268b6d763c52a366dad1ff66bbf
Artem <progerguru@gmail.com>
2025-06-08 11:49:54 +0300

    chore: move requirements.txt to main dir

--- backend/main.py
+++ backend/main.py
@@ -7,13 +7,13 @@ from api import router as api_router
 app = FastAPI()
 app.include_router(api_router)
 
-# app.add_middleware(
-# 	CORSMiddleware,
-# 	allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-# 	allow_credentials=True,
-# 	allow_methods=["*"],
-# 	allow_headers=["*"],
-# )
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 if __name__ == "__main__":
 	uvicorn.run(
